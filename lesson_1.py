a827fd57cbec7c4b1ee18086e4c9789cd2eabbb8
Ðdootkulovjoomart <dootkulovjoomart@gmail.com>
2025-12-30 23:06:09 +0600

    lesson_1

---
+++ lesson_1.py
@@ -0,0 +1,15 @@
+class Hero:
+    # конструктор класса
+    def __init__(self, name , lvl , hp ):
+
+        # атриьуты класса
+        self.name = name
+        self.lvl = lvl
+        self.hp = hp
+    def action(self):
+        return f'I am {self.name} base action !!'
+#Обьект
+kirito = Hero("Ardager", 100, 1000)
+tomy = Hero("Tom", 2000, 3000)
+print(tomy.lvl)
+print(tomy.action()) 

120ed6da2b46f2cffb73c432f463c4502da86080
dootkulovjoomart-prog <dootkulovjoomart@gmail.com>
2026-01-13 18:45:56 +0600

    main

---
+++ hw/main.py
@@ -0,0 +1,7 @@
+# Библиотека: rich
+# Назначение: красивый вывод текста в консоль
+# Применение: используется в ООП-проектах для отображения информации об объектах
+from rich.console import Console
+console = Console()
+
+console.print("zhoomart", style="bold red")
